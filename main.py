cabdbfcb776f7cba5060910f25dc6395232d501d
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2026-04-10 12:19:59 +0200

    copied password

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from rich.progress import Progress, SpinnerColumn, TextColumn
 some_other = "hydsdawqh8ddsadYR1i2srLYdKa" 
 API_KEY = "1@qwerty" 
 SOME_PASSWORD = "1@qwerty" 
+mySecret = "hyddYR1i2srLYdKa"
 
 
 def get_ingredients(recipe_file: Path) -> list[str]:
